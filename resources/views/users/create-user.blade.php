1a53193eac7a3eb339418c144067755a1f2c6183
baletify <iqbalpanggung@gmail.com>
2025-05-13 08:54:40 +0800

    update user role and input type

--- resources/views/users/create-user.blade.php
+++ resources/views/users/create-user.blade.php
@@ -71,29 +71,41 @@
               </div>
             </div>
             <div class="relative mt-1 rounded-md">
+              <div class="">
                 <span class="pl-1 font-semibold">Input Type</span>  
-                <select name="input_type" id="input_type" class="col-start-1 row-start-1 w-full appearance-none rounded-md py-1.5 pl-3 pr-7 text-base text-gray-500 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                    <option value="">-- Input Type --</option>
-                    <option value="Individual">Individual</option>
-                    <option value="Group">Group</option>
-                </select>
+              </div>
+              <div class="pl-1">
+                <input type="radio" name="input_type" id="input_type" value="Individual" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                <label for="input_type" class="ml-2 text-sm font-medium text-gray-900">Individual</label>
+                <input type="radio" name="input_type" id="input_type" value="Group" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 ml-4">
+                <label for="input_type" class="ml-2 text-sm font-medium text-gray-900">Group</label>
+              </div>
               <div class="absolute inset-y-0 right-0 flex items-center">
               </div>
             </div>
             <div class="relative mt-1 rounded-md">
-                <span class="pl-1 font-semibold">Role</span>
-                <select name="role" id="role" class="col-start-1 row-start-1 w-full appearance-none rounded-md py-1.5 pl-3 pr-7 text-base text-gray-500 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                <option value="">-- Role --</option>
-                <option value="">Individu</option>
-                <option value="Inputer">Inputer</option>
-                <option value="Checker 1">Check 1</option>
-                <option value="Checker 2">Check 2</option>
-                <option value="Mng Approver">Mng Approver</option>
-                <option value="Approver">Approver</option>
-                </select>
-                <div class="absolute inset-y-0 right-0 flex items-center">
+              <div class="">
+                <span class="pl-1 font-semibold">Role</span>  
+              </div>
+              <div class="pl-1">
+                <div class="">
+                  <input type="radio" name="role" id="role" value="Inputer" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                  <label for="role" class="ml-2 text-sm font-medium text-gray-900">Inputer</label>
+                  <input type="radio" name="role" id="role" value="Checker 1" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 ml-4">
+                  <label for="role" class="mx-2 text-sm font-medium text-gray-900">Check 1</label>
+                  <input type="radio" name="role" id="role" value="Checker 2" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                  <label for="role" class="ml-2 text-sm font-medium text-gray-900">Check 2</label>
+                </div>
+                <div class="">
+                  <input type="radio" name="role" id="role" value="Mng Approver" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                  <label for="role" class="ml-2 text-sm font-medium text-gray-900">Mng Approver {{ '(Mng)' }}</label>
+                  <input type="radio" name="role" id="role" value="Approver" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 ml-4">
+                  <label for="role" class="ml-2 text-sm font-medium text-gray-900">Approver {{ '(HRD)' }}</label>
                 </div>
               </div>
+              <div class="absolute inset-y-0 right-0 flex items-center">
+              </div>
+            </div>
             </div>
             <div class="flex mt-5 gap-x-3 justify-center">
                 <button type="button" class="px-4 py-2 bg-red-600 text-white rounded-md" onclick="history.back();">
